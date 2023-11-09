3e542390330b556e75c3678051090d946a50a241
sxxkxx <seuld1521@gmail.com>
2023-11-09 10:38:25 +0900

    컨트롤러 수정

--- src/main/java/com/example/demo/CustomerController.java
+++ src/main/java/com/example/demo/CustomerController.java
@@ -3,17 +3,27 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.accessingdatajpa.Customer;
 import com.example.demo.accessingdatajpa.CustomerRepository;
 
-@RestController //Controller + ResponseBody
+@RestController // Controller + ResponseBody
 public class CustomerController {
-	
-	@Autowired CustomerRepository repo;
+
+	@Autowired
+	CustomerRepository repo;
 
 	@GetMapping("/customer")
-	public Iterable<Customer> findall(){
+	public Iterable<Customer> findall() {
 		return repo.findAll();
+	}
+
+	@GetMapping("/")
+	public ModelAndView main() {
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("greet", "<b>안녕</b>하세요");
+		return mv;
+
 	}
 }
